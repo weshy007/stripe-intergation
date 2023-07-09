f3963c7d1308d048d99e0161783daa42a9bd8263
Weshy Joe <josephwaweru96@gmail.com>
2023-07-10 00:12:10 +0300

    tenplates

--- payments/urls.py
+++ payments/urls.py
@@ -3,5 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path('', views.product_create, name='create'),
+    path('edit/<id>/', views.product_edit, name='edit'),
+    path('detail/<id>/', views.PaymentConfirmation.as_view(), name='detail'),
+    path('success/', views.payment_success, name='success'),
+    path('failed/', views.PaymentFailedView.as_view(), name='failed'),
+    path('history/', views.OrderHistoryListView.as_view(), name='history'),
+    path('api/checkout-session/<id>/', views.create_checkout_session, name='api_checkout_session'),
 ]
--- payments/views.py
+++ payments/views.py
@@ -1,8 +1,9 @@
 import json
 
+from decimal import Decimal
 import stripe
 from django.conf import settings
-from django.http.response import HttpResponseNotFound, JsonResponse
+from django.http.response import HttpResponseNotFound, JsonResponse, HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
 from django.views.decorators.csrf import csrf_exempt
@@ -14,7 +15,7 @@ from .models import OrderDetail, Product
 # Create your views here.
 class PaymentConfirmation(DetailView):
     model = Product
-    template_name = 'payment.html'
+    template_name = 'payment_detail.html'
     pk_url_kwarg = 'id'
 
     def get_context_data(self, **kwargs):
@@ -28,40 +29,42 @@ def create_checkout_session(request, id):
     request_data = json.loads(request.body)
     product = get_object_or_404(Product, pk=id)
 
-    stripe.api_key = settings.STRIPE_API_KEY
+    stripe.api_key = settings.STRIPE_SECRET_KEY
     checkout_session = stripe.checkout.Session.create(
-        customer_email=request_data['email'],
+        # Customer Email is optional,
+        # It is not safe to accept email directly from the client side
+        customer_email = request_data['email'],
         payment_method_types=['card'],
         line_items=[
             {
                 'price_data': {
                     'currency': 'usd',
                     'product_data': {
-                        'name': request.user
+                    'name': request.user,
                     },
-
-                    'unit_amount': int(product.price * 100)
+                    
+                    'unit_amount': int(product.price * 100),
                 },
-                'quantity': 1
+                'quantity': 1,
             }
         ],
         mode='payment',
-        success_url=request.build_absolute_url(
+        success_url=request.build_absolute_uri(
             reverse('success')
         ) + "?session_id={CHECKOUT_SESSION_ID}",
-        cancel_url=request.build_absolute_url(reverse('failed'))
+        cancel_url=request.build_absolute_uri(reverse('failed')),
     )
 
     order = OrderDetail()
     order.customer_email = request_data['email']
     order.product = product
-    order.stripe_payment_intent = create_checkout_session['id']
+    order.stripe_payment_intent = checkout_session['id']
     order.amount = int(product.price)
     order.save()
 
     print("checkout_session ==============", checkout_session)
 
-    return JsonResponse({'session_id': create_checkout_session})
+    return JsonResponse({'session_id': checkout_session['id']})
 
 
 def payment_success(request):
@@ -84,14 +87,15 @@ def payment_success(request):
     return render(request, 'payment_success.html', context)
 
 def product_create(request):
-    if request.method == 'POST':
-        amount = request.POST.get('amount')
+    if request.method == "POST":
+        amount = Decimal(request.POST.get("amount"))
         product = Product.objects.create(price=amount)
 
         print("Product ID ========", product)
         return redirect("detail", product.id)
-
-    return render('detail', 'payment_create.html')
+    
+   
+    return render(request, "payment_create.html")
 
 
 def product_edit(request, id):
@@ -105,7 +109,7 @@ def product_edit(request, id):
         return redirect("detail", id)
     
     context = {
-        "product":product
+        "product": product
     }
     
     return render(request, "payment_edit.html", context)
